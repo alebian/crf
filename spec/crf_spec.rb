51d99dcba4a4c1eefe0cd3fccae071ffcb2fb4c9
alebian <alebezdjian@gmail.com>
2016-03-19 10:50:56 -0300

    Test human readable size

--- spec/crf_spec.rb
+++ spec/crf_spec.rb
@@ -8,6 +8,7 @@ describe 'Crf' do
     ["#{test_files_directory}/file_1.test", "#{test_files_directory}/file_2.test"]
   end
   let!(:repeated_text) { 'This text will be in 2 files' }
+
   before do
     FileUtils.rm_rf(test_files_directory) if File.exist?(test_files_directory)
     Dir.mkdir(test_files_directory) unless File.exist?(test_files_directory)
@@ -15,19 +16,46 @@ describe 'Crf' do
       File.open(file_path, 'w+') { |file| file.write(repeated_text) }
     end
   end
+
   context 'when using the checker' do
     before do
       checker.check_repeated_files
     end
+
     it 'creates the logger' do
       expect(checker.logger.nil?).to be_falsey
     end
+
     it 'finds repetitions' do
       expect(checker.repetitions.empty?).to be_falsey
     end
+
     it 'deletes the repetitions' do
       checker.check_repeated_files
       expect(checker.repetitions.empty?).to be_truthy
+    end
+  end
+
+  describe '#number_to_human_size' do
+    let(:bytes) { 300 }
+    let(:kbytes) { 3_000 }
+    let(:mbytes) { 3_000_000 }
+    let(:gbytes) { 3_000_000_000 }
+
+    it 'prints bytes' do
+      expect(checker.send(:number_to_human_size, bytes)).to eq('300 bytes')
+    end
+
+    it 'prints KB' do
+      expect(checker.send(:number_to_human_size, kbytes)).to eq('2.93 KB')
+    end
+
+    it 'prints MB' do
+      expect(checker.send(:number_to_human_size, mbytes)).to eq('2.86 MB')
+    end
+
+    it 'prints GB' do
+      expect(checker.send(:number_to_human_size, gbytes)).to eq('2.79 GB')
     end
   end
 end
